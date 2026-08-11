e623e0729e537a700a092b524ec197f14fb8730c
Phillip Chin <ekkus93@gmail.com>
2026-08-10 18:22:22 -0700

    fix(android): restore P2 instrumentation compilation

--- app/src/androidTest/java/com/ekkus/silentdisco/P2UiTest.kt
+++ app/src/androidTest/java/com/ekkus/silentdisco/P2UiTest.kt
@@ -1,6 +1,5 @@
 package com.ekkus.silentdisco
 
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
